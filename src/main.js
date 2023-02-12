531d7ccc3092a2fb3858e0c3256ac260e88d3c01
zhenglei <mexiaoyao@163.com>
2023-02-12 21:52:24 +0800

    加入Ant Design of Vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'ant-design-vue/dist/antd.css';
+
 console.log('版本:'+ require('../package.json').version)
 
+//开发环境下，Vue 会提供很多警告来帮你对付常见的错误与陷阱。 而在生产环境下，这些警告语句却没有...
 Vue.config.productionTip = false
+
+Vue.use(Antd);
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button type="primary">
+      Primary
+    </a-button>
+    <a-icon type="stock" />
+    <a-space>
+    Space
+    <a-button type="primary">Button</a-button>
+    <a-upload>
+      <a-button> <a-icon type="upload" /> Click to Upload </a-button>
+    </a-upload>
+    <a-popconfirm title="Are you sure delete this task?" ok-text="Yes" cancel-text="No">
+      <a-button>Confirm</a-button>
+    </a-popconfirm>
+  </a-space>
   </div>
 </template>
