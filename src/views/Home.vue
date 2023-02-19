95c71cec277a98aa212901c8d9a8a3e31c47ba14
zhenglei <mexiaoyao@163.com>
2023-02-19 22:50:55 +0800

    动态字体

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,12 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <span @click="changeLocale()">语言切换</span>
     {{$t("open")}}
-      <div style="width: 100px; height: 120px;"><LetterTable :backColorList="cCss"/></div>
+    <div>
+      <div style="width: 100px; height: 120px; float: left;"><LetterTable :backColorList="cCss"/></div>
+      <div style="width: 100px; height: 120px; float: left; margin-left: 15px;"><LetterTable :backColorList="sCss"/></div>
+      <div style="width: 100px; height: 120px; float: left; margin-left: 15px;"><LetterTable :backColorList="uCss"/></div>
+
+    </div>
     
   </div>
 </template>
@@ -36,15 +41,71 @@ export default {
         "#707070", //灰色3
         "#707070", //灰色4
         "#00fe00", //银光绿6
-        "#707070", //红色10
+        "#707070", //灰色10
         "#707070", //灰色11
         "#707070", //灰色16
         "#707070", //灰色21
-        "#ff0101", //灰色26
+        "#ff0101", //红色26
         "#707070", //灰色30
         "#707070", //灰色32
         "#707070", //灰色33
         "#0001fc", //蓝色34
+      ],
+
+      sCss:[],
+      sNumList: [
+        2,3,4,5,
+        9,
+        13,
+        17,18,19,
+        25,
+        26,30,
+        32,33,34
+      ],
+      sColorList: [
+        "#707070", //灰色2
+        "#707070", //灰色3
+        "#707070", //灰色4
+        "#0001fc", //蓝色5
+        "#707070", //灰色9
+        "#707070", //灰色13
+        "#ff0101", //红色17
+        "#707070", //灰色18
+        "#707070", //灰色19
+        "#707070", //灰色25
+        "#00fe00", //银光绿26
+        "#707070", //灰色30
+        "#707070", //灰色32
+        "#707070", //灰色33
+        "#707070", //灰色34
+      ],
+
+      uCss:[],
+      uNumList: [
+        1,5,
+        6,10,
+        11,15,
+        16,20,
+        21,25,
+        26,30,
+        32,33,34
+      ],
+      uColorList: [
+        "#707070", //灰色1
+        "#00fe00", //银光绿5
+        "#707070", //灰色6
+        "#707070", //灰色10
+        "#707070", //灰色11
+        "#707070", //灰色15
+        "#707070", //灰色16
+        "#707070", //灰色20
+        "#707070", //灰色21
+        "#707070", //灰色25
+        "#707070", //灰色26
+        "#707070", //灰色30
+        "#ff0101", //红色32
+        "#707070", //灰色33
+        "#707070", //灰色34
       ],
 
     }
@@ -55,15 +116,25 @@ export default {
   methods: {
     createC() {
       this.cCss = [];
+      this.sCss = [];
+      this.uCss = [];
+
       this.createLetter(this.cNumList, this.cColorList, this.cCss);
-      this.createCmake(this.cNumList, this.cColorList, this.cCss);
+      this.createMake(this.cNumList, this.cColorList, this.cCss);
+
+      this.createLetter(this.sNumList, this.sColorList, this.sCss);
+      this.createMake(this.sNumList, this.sColorList, this.sCss);
+
+      this.createLetter(this.uNumList, this.uColorList, this.uCss);
+      this.createMake(this.uNumList, this.uColorList, this.uCss);
     },
-    createCmake(cNumList, cColorList, cCss){
-      cCss = [];
+
+    createMake(numList, colorList, css){
       setTimeout(()=>{
-        this.createLetter(cNumList, cColorList, cCss);
-        cColorList.unshift(cColorList.pop());
-        this.createCmake(cNumList, cColorList);
+        css.splice(0, css.length);
+        this.createLetter(numList, colorList, css);
+        colorList.unshift(colorList.pop());
+        this.createMake(numList, colorList, css);
       }, this.time);
     },
     createLetter(letter, color, letterCss){
