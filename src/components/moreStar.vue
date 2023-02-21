351e7b45afdeed399e733b590c9d1221ea11f979
zhengxiaolei <zhengxiaolei@vrvmail.com.cn>
2023-02-21 18:39:38 +0800

    首页浮动点颜色

--- src/components/moreStar.vue
+++ src/components/moreStar.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="starBody">
-      <!-- <div class="star" ref="starsRef"></div> -->
       <div class="meteor" ref="metreorRef" v-for="(item, index) in meteors" :key="index"></div>
   </div>
 </template>
@@ -17,6 +16,15 @@ export default {
       starWidth: 1920, //黑夜边框
       starHeight: 1080, //黑夜边框
       meteors: 100,  // 流星个数
+
+      colorList: [
+        "#00fe00", //灰色
+        "#ffffff", //银光绿6
+        "#0001fc", //蓝色34
+        "#ff0101", //红色26
+        "#00fe00", //灰色
+        "#ffffff", //白色
+      ]
     }
   },
   computed: {
@@ -31,22 +39,9 @@ export default {
         this.starHeight = document.body.clientHeight;
       })();
     };
-    //this.createStar();
     this.createmetreor();
   },
   methods: {
-    //创建星星
-    createStar(){
-      let starsRef = this.$refs.starsRef;
-      let shadow = ""
-      for (let i = 0; i < this.starts; i++) {
-        let x = Math.floor(Math.random() * this.starWidth);
-        let y = Math.floor(Math.random() * this.starHeight);
-        shadow = `${x}px ${y}px #fff, ${shadow}`
-      }
-      shadow = shadow.substring(0, shadow.length - 2)
-      starsRef.style.boxShadow = shadow
-    },
     createmetreor(){
       let metreorRef = this.$refs.metreorRef;
       metreorRef.forEach((item) => {
@@ -55,6 +50,8 @@ export default {
         let time = Math.floor(Math.random() * 800 + 2); //速度 值越大越慢
         item.style.left = `${v}%`;
         item.style.top = `${h}px`;
+        let num = Math.trunc(Math.random() * 6);
+        item.style.backgroundColor = this.colorList[num];
         item.style.animation = `meteorAnim ${time}s linear  infinite`
       })
     }
@@ -62,7 +59,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less">
 @keyframes meteorAnim {
   0% {
@@ -93,17 +89,10 @@ export default {
   }
   .meteor{
     position: absolute;
-    width: 3px;
-    height: 3px;
+    width: 5px;
+    height: 5px;
     top: 10px;
     left:30px;
-  }
-  .meteor::before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: rgba(#fff,1);
   }
 }
 </style>
